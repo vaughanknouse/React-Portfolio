46abd9ec296c53b7912657ae49597be362c3cc27
Vaughan Knouse <vaughanknouse@gmail.com>
2024-10-20 20:16:43 -0400

    added react-router-dom and bootstrap dependencies, also added comments to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'; // Import the defineConfig function from Vite
+import react from '@vitejs/plugin-react'; // Import the React plugin for Vite
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Define the plugins to be used by Vite
   plugins: [react()],
+  // Server configuration
   server: {
-    port: 3000,
-    open: true,
+    port: 3000, // Set the server port to 3000
+    open: true, // Automatically open the browser when the server starts
   },
 });
